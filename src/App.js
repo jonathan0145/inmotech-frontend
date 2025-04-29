fa076fd9f8b38ef4e8ca01fab0220d8826e01c8d
jonathan0145 <jonathan_bermeo014@hotmail.com>
2025-04-29 15:49:08 -0500

    agregue comentario en app.js de lo que debo hacer para el login

--- src/App.js
+++ src/App.js
@@ -15,3 +15,15 @@ function App() {
 }
 
 export default App;
+
+/*
+  necesito arreglar las rutas donde al ingresar a la pagina lo primero que se pueda hacer es iniciar seccion
+  * deben haber dos botones uno para iniciar seccion y otro para registrarse
+    *en iniciar seccion 
+      -debe haber un formulario para iniciar seccion
+      -un boton para iniciar seccion que me lleve a inicio
+    *en registrarse
+      -debe haber un formulario donde se pueda ingresar informacion para hacer login y perfil
+      -debe haber un boton que diga registrarse y al hacer clic registre al usuario y haga a la vez lo que hace el boton iniciar seccion es decir renderizar ese componente para hacer login
+    ? investigar que otras cosas debe tener el login e iniciar seccion
+*/
